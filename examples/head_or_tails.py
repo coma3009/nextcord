73f86b326cd80cc98a62d7d77c98e95250b5c728
Coma <75559250+coma3009@users.noreply.github.com>
2022-06-04 13:51:25 +0900

    Add files via upload

---
+++ examples/head_or_tails.py
@@ -0,0 +1,22 @@
+# This is an example for heads or tails
+# It is a simple game where the user has to guess
+# if the coin flip will result in heads or tails
+
+# example: $headortails heads
+
+import random
+
+from nextcord.ext import commands
+
+bot = commands.Bot(command_prefix="$")
+
+
+@bot.command()
+async def headortails(ctx, answer):
+    if random.choice(["heads", "tails"]) == answer:
+        await ctx.reply("Congratulations")
+    else:
+        await ctx.reply("Sorry you lost")
+
+
+bot.run("token")
---
+++ examples/reply.py
@@ -0,0 +1,11 @@
+from nextcord.ext import commands
+
+bot = commands.Bot(command_prefix="$")
+
+
+@bot.command()
+async def hello(ctx):
+    await ctx.reply("Hello!")
+
+
+bot.run("token")
